cace9ab644ccf27fce41574a78f0627ab55b5065
elizabeth <jelizabethpardo@gmail.com>
2020-08-31 21:46:23 -0300

    Cambios en Main

--- app/src/main/java/com/example/trabajopractico1_ep/MainActivity.java
+++ app/src/main/java/com/example/trabajopractico1_ep/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.trabajopractico1_ep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private UsbConnected usbConnected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        usbConnected = new UsbConnected();
+        registerReceiver(usbConnected, new IntentFilter("android.hardware.usb.action.USB_STATE"));
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(usbConnected);
     }
 }
